50a453269308e33bfbd7692c32377e3c5cd1b527
Mario Murillo <mardres@gmail.com>
2020-06-28 17:21:33 -0500

    Feat: remove unnecesary comments

--- src/main/java/com/example/demospring/usecase/UsuarioService.java
+++ src/main/java/com/example/demospring/usecase/UsuarioService.java
@@ -17,14 +17,3 @@ public class UsuarioService {
     repository.save(usuario);
   }
 }
-/*
-com.mysql.jdbc.Driver
-jdbc:mysql://localhost:3306/mysqltutorial?useSSL=false
-
-spring.jpa.hibernate.ddl-auto=update
-spring.datasource.url=jdbc:mysql://${MYSQL_HOST:localhost}:3306/db_example
-spring.datasource.username=springuser
-spring.datasource.password=ThePassword
-
-spring.jpa.properties.hibernate.dialect = org.hibernate.dialect.MySQL5Dialect
- */
